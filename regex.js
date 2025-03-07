d12d78c260137282648e4fc7d0ec9278a6141025
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-03-07 18:55:36 +0530

    [Kuldeep] add. Email validation pattern of uc1

---
+++ regex.js
@@ -0,0 +1,38 @@
+function validatePin(pin) {
+    const pinPattern = /^(?![A-Za-z])\d{3}\s?\d{3}(?![A-Za-z])$/;
+    if (pinPattern.test(pin)) {
+        console.log(`Valid PIN: ${pin}`);
+        return true;
+    } else {
+        console.error(`Invalid PIN: ${pin}`);
+        return false;
+    }
+}
+
+//PIN Code UC1
+validatePin("400088"); 
+
+//PIN Code UC2
+validatePin("A400088");
+
+//PIN Code UC3
+validatePin("400088B"); //Invalid Pin 
+
+//PIN Code UC4
+validatePin("400 088"); //Valid Pin
+
+// Function to validate email address
+function validateEmail(email) {
+    // Regular expression for full email validation (UC-1 to UC-5)
+    const emailPattern = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-z]{2})?$/;
+
+    // Checking if email matches the pattern
+    if (emailPattern.test(email)) {
+        console.log(`Valid Email: ${email}`);
+    } else {
+        console.log(`Invalid Email: ${email}`);
+    }
+} 
+
+// Email UC-1 
+validateEmail("abc@bridgelabz.co"); //Valid Email
